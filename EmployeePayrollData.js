8218d75b0ba025eb4aa8014feea5fa660d6f11bd
Prashik Kamble <kambleprashik7349@gmail.com>
2023-01-11 16:14:43 +0530

    ADDED : Code To Validate Name Using Regex And Try Catch

--- EmployeePayrollData.js
+++ EmployeePayrollData.js
@@ -16,20 +16,30 @@ class EmployeePayrollData {
 
     //getter and setter method
     get name() {return this._name;}
-    set name(name) {this._name = name;}
+    set name(name) {
+        let nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$');
+        if(nameRegex.test(name))
+            this._name = name;
+        else throw 'Name is Incorrect!';
+        }
 
     //method
     toString() {
         const options = {year : 'numeric', month : 'long', day : 'numeric'};
-        const empDate = this.startDate === undefined ? "undefined" : this.startDate.toLocaleDateString("en-US", options);
+        const empDate = !this.startDate ? "undefined" : this.startDate.toLocaleDateString("en-US", options);
         return "id=" + this.id + ", name=" + this.name + ", salary=" + this.salary + ", " +
                "gender=" + this.gender + ", startDate=" + empDate;
     }
 }
 let employeePayrollData = new EmployeePayrollData(1, "Mark", 30000);
 console.log(employeePayrollData.toString());
-employeePayrollData.name = "John";
-console.log(employeePayrollData.toString());
+
+try{
+    employeePayrollData.name = "john";
+    console.log(employeePayrollData.toString());
+} catch(e) {
+    console.error(e);
+}
 
 let newEmployeePayrollData = new EmployeePayrollData(1, "Terrisa", 30000, "F", new Date());
 console.log(newEmployeePayrollData.toString());
