9d5cf45046c8bbd85ea1d8f69c7f5c29f869258e
Prashik Kamble <kambleprashik7349@gmail.com>
2023-01-11 15:59:49 +0530

    ADDED : Code To Store Gender And StartDate In Existing Employee Payroll Data

--- EmployeePayrollData.js
+++ EmployeePayrollData.js
@@ -2,12 +2,16 @@ class EmployeePayrollData {
     //property
     id;
     salary;
+    gender;
+    startDate;
     
     //constructor
-    constructor(id, name, salary) {
-        this.id = id;
-        this.name = name;
-        this.salary = salary;
+    constructor(...params) {
+        this.id = params[0];
+        this.name = params[1];
+        this.salary = params[2];
+        this.gender = params[3];
+        this.startDate = params[4];
     }
 
     //getter and setter method
@@ -16,10 +20,16 @@ class EmployeePayrollData {
 
     //method
     toString() {
-        return "id=" + this.id + ", name=" + this.name + ", salary=" + this.salary;
+        const options = {year : 'numeric', month : 'long', day : 'numeric'};
+        const empDate = this.startDate === undefined ? "undefined" : this.startDate.toLocaleDateString("en-US", options);
+        return "id=" + this.id + ", name=" + this.name + ", salary=" + this.salary + ", " +
+               "gender=" + this.gender + ", startDate=" + empDate;
     }
 }
 let employeePayrollData = new EmployeePayrollData(1, "Mark", 30000);
 console.log(employeePayrollData.toString());
 employeePayrollData.name = "John";
 console.log(employeePayrollData.toString());
+
+let newEmployeePayrollData = new EmployeePayrollData(1, "Terrisa", 30000, "F", new Date());
+console.log(newEmployeePayrollData.toString());
